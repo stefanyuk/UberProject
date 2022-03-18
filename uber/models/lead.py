586f6da38c5d25cbf6cbcb34e535a82555f3d28c
vOIEnRkw <stefanyuk.andriyigor@gmail.com>
2022-03-18 11:03:38 +0100

    added mask for phone_input

--- uber/models/lead.py
+++ uber/models/lead.py
@@ -2,6 +2,7 @@ from uber import db
 
 
 class Lead(db.Model):
+    """Model to save Lead information to the database"""
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     phone_number = db.Column(db.String)
@@ -9,6 +10,7 @@ class Lead(db.Model):
 
     @classmethod
     def create(cls, data):
+        """Creates new lead record in the db"""
         lead = cls(**data)
         db.session.add(lead)
         db.session.commit()
